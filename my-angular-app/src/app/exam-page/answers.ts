1a5768f048e83e5c5d83893f723de01079e9ff35
MuCaglayan <emincaglayan027@gmail.com>
2024-08-28 11:02:38 +0300

    css update and new questions

--- my-angular-app/src/app/exam-page/answers.ts
+++ my-angular-app/src/app/exam-page/answers.ts
@@ -10,17 +10,35 @@ export class Answers {
         // Her soru için bir obje dizisi tanımlanıyor
         this.options = [
             { question: 'Türkiye’nin başkenti neresidir?',
-              choices: ['İstanbul', 'Ankara', 'İzmir', 'Antalya' ],
-              correctAnswer: 'Ankara' },
+               choices: ['İstanbul', 'Ankara', 'İzmir', 'Antalya' ],
+               correctAnswer: 'Ankara' },
             { question: 'Dünyanın en yüksek dağı hangisidir?',
-              choices: ['Everest', 'Kilimanjaro', 'McKinley', 'Elbrus'],
-              correctAnswer: 'Everest' },
+               choices: ['Everest', 'Kilimanjaro', 'McKinley', 'Elbrus'],
+               correctAnswer: 'Everest' },
             { question: 'Hangi gezegen Güneş Sistemi’ndeki en büyük gezegendir?',
-              choices: [ 'Jüpiter', 'Satürn'],
-              correctAnswer: 'Jüpiter' },
+               choices: [ 'Mars','Jüpiter', 'Satürn'],
+               correctAnswer: 'Jüpiter' },
             { question: 'Dünyanın en derin okyanusu hangisidir?',
-              choices: ['Atlantik', 'Hint', 'Pasifik', 'Kuzey Buz Denizi'],
-              correctAnswer: 'Kuzey Buz Denizi' }
+               choices: ['Atlantik', 'Hint', 'Pasifik', 'Kuzey Buz Denizi'],
+               correctAnswer: 'Pasifik' },
+            { question: 'Hangi canlı daha ağırdır?',
+               choices: ['Fil', 'Aslan', 'Zürafa', 'Mavi Balina' ],
+               correctAnswer: 'Mavi Balina' },
+            { question: 'Dünyanın en uzun nehri hangisidir?',
+               choices: ['Amazon Nehri', 'Nil Nehri', 'Fırat Nehri', 'Yangtze Nehri' ],
+               correctAnswer:'Nil Nehri' },
+            { question: 'Mona Lisa tablosunu kim yapmıştır?',
+               choices: ['Leonardo da Vinci', 'Vincent van Gogh', 'Pablo Picasso', 'Michelangelo Buonarroti' ],
+               correctAnswer:'Leonardo da Vinci' },
+            { question: 'Sefiller kiabının yazarı kimdir?',
+               choices: ['Fyodor Dostoevsky', 'William Shakespeare', 'Leo Tolstoy', 'Victor Hugo' ],
+               correctAnswer:'Victor Hugo' },
+           { question: 'Tarihte bilinen ilk yazı hangi uygarlık tarafından bulunmuştur?',
+                choices: ['Mısır', 'Çin', 'Sümerler', 'Yunan'],
+                correctAnswer: 'Sümerler' },
+            { question: 'Avrupa Birliği’nin para birimi nedir?',
+                choices: ['Dolar', 'Euro', 'Sterlin', 'Frank'],
+                correctAnswer: 'Euro' }
         ];
 
         // RowController'ı, soru sayısına göre başlatıyoruz
